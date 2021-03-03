467e367deb24d0ee6eb02a6185f89bef8d0e466a
Emi-A <emilyam23@gmail.com>
2021-03-03 18:42:17 +0000

    subiendo

---
+++ src/js/component/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Navbar } from "./navbar.js";
+import { Jumbotron } from "./jumbotron.js";
+
+export function Main() {
+	return (
+		<div>
+			<Navbar />
+
+			<Jumbotron />
+		</div>
+	);
+}
