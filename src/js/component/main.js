53ad319b8b986663854d57d93489493fcced37d4
Emi-A <emilyam23@gmail.com>
2021-03-04 16:52:46 +0000

    cargando

--- src/js/component/main.js
+++ src/js/component/main.js
@@ -2,13 +2,32 @@ import React from "react";
 
 import { Navbar } from "./navbar.js";
 import { Jumbotron } from "./jumbotron.js";
+import { Card } from "./card.js";
 
 export function Main() {
 	return (
 		<div>
 			<Navbar />
-
-			<Jumbotron />
+			<div className="container">
+				<Jumbotron />
+			</div>
+			<div className="container">
+				<div className="row align-items-start">
+					<div className="col">
+						<Card />
+					</div>
+				</div>
+				<div className="row align-items-center">
+					<div className="col">
+						<Card />
+					</div>
+				</div>
+				<div className="row align-items-end">
+					<div className="col">
+						<Card />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
